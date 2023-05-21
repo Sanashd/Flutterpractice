007a558eaea4b79b04a614ddad9fd281f54368d1
Sana Shahid <sanashd20@gmail.com>
2023-05-22 03:57:23 +0500

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/home_page.dart';
+import 'package:flutter_demo/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,3 +22,4 @@ class HomePage extends StatelessWidget {
         ));
   }
 }
+
